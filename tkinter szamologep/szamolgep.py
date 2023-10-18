750e9da4ea4afefa35deafb888604ad6c87d6326
JaniBT <janoscsordas072@gmail.com>
2023-10-18 14:21:45 +0200

    Update szamolgep.py

--- tkinter szamologep/szamolgep.py
+++ tkinter szamologep/szamolgep.py
@@ -1,31 +1,37 @@
 import math
 
-
+status = True
+while status:
     try:
-            print("\n================================ (ENTER leütésével léphetsz ki)")
+            print("\n================================ (69 leütésével léphetsz ki)")
             print("Adj meg egy számot: ")
             szam1 = int(input())
             print("Adj meg egy másik számot: ")
             szam2 = int(input())
             print("Add meg, hogy kivonni, összeadni, szorozni vagy osztani szeretnéd: ")
             muveletijel = input()
-            while szam1 != "q":
-                if szam1 != int or szam2 != int or muveletijel != "*" or muveletijel != "/" or muveletijel != "+" or muveletijel != "-":
-                    if muveletijel == "+":
-                        print(f'Az eredmény: {szam1+szam2}')
-                    elif muveletijel == "-":
-                        print(f'Az eredmény: {szam1-szam2}')
-                    elif muveletijel == "*":
-                        print(f'Az eredmény: {szam1*szam2}')
-                    else:
-                        print(f'Az eredmény: {szam1/szam2}')
-                        
-                    print("\n================================ (ENTER leütésével léphetsz ki)")
-                    print("Adj meg egy számot: ")
-                    szam1 = int(input())
-                    print("Adj meg egy másik számot: ")
-                    szam2 = int(input())
-                    print("Add meg, hogy kivonni, összeadni, szorozni vagy osztani szeretnéd: ")
-                    muveletijel = input()
+            if szam1 != "q":
+                while szam1 != "q":
+                    if szam1 != int or szam2 != int or muveletijel != "*" or muveletijel != "/" or muveletijel != "+" or muveletijel != "-":
+                        if muveletijel == "+":
+                            print(f'Az eredmény: {szam1+szam2}')
+                        elif muveletijel == "-":
+                            print(f'Az eredmény: {szam1-szam2}')
+                        elif muveletijel == "*":
+                            print(f'Az eredmény: {szam1*szam2}')
+                        else:
+                            print(f'Az eredmény: {szam1/szam2}')
+                        print("\n================================ (69 leütésével léphetsz ki)")
+                        print("Adj meg egy számot: ")
+                        szam1 = int(input())
+                        print("Adj meg egy másik számot: ")
+                        szam2 = int(input())
+                        print("Add meg, hogy kivonni, összeadni, szorozni vagy osztani szeretnéd: ")
+                        muveletijel = input()
+                    elif szam1 == "q":
+                        status = False
+            elif szam1 == "q":
+                status = False
+                print(status)
     except ValueError:
             print("Csak adott karaktereket adhatsz meg!")
