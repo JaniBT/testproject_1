7aa92b888855ea3083c98b3a4f7ee8dcbba2edcf
JaniBT <janoscsordas072@gmail.com>
2023-10-20 08:01:11 +0200

    Update mondat.py

--- mondat.py
+++ mondat.py
@@ -1,12 +1,16 @@
 print('A program a mondatvégi írásjelből kitalálja minden mondatot írtál!\n')
 mondat = input('Adj meg egy mondatot: ')
 
-if mondat[-1] == '!':
-    print('A mondat felszólító/óhajtó/felkiáltó mondat.')
-elif mondat[-1] == '?':
-    print('A mondat kérdő mondat')
-elif mondat[-1] == '.':
-    print('A mondat kijelentő mondat')
-else:
-    print('Nem adtál meg mondatvégi írásjelet!')
+while mondat != '':
+    if mondat[-1] == '!':
+        print('A mondat felszólító/óhajtó/felkiáltó mondat.')
+    elif mondat[-1] == '?':
+        print('A mondat kérdő mondat')
+    elif mondat[-1] == '.':
+        print('A mondat kijelentő mondat')
+    else:
+        print('Nem adtál meg mondatvégi írásjelet!')
+        
+    print('A program a mondatvégi írásjelből kitalálja minden mondatot írtál!\n')
+    mondat = input('Adj meg egy mondatot: ')
 
